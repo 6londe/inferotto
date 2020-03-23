efbb4ea557d9a648e74a326966475067d387f122
6londe <6londe@gmail.com>
2020-03-24 00:32:45 +0900

    Fix model

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import json
 import requests
 import time
 import numpy as np
+import keras
 from keras import models, layers
 from sklearn.model_selection import train_test_split
 
@@ -30,28 +31,36 @@ try:
         drwNo = drwNo + 1
 
 finally:
-    print("Train with " + str(len(data)) + " data..")
+    print("Train with last " + str(len(data)) + " data..")
 
     x = []
     y = []
     for i in range(1, drwNo):
         x.append(i)
-        refined = []
+        refined = np.zeros(45)
         for drwt in range(1, 7):
-            refined.append(data[str(i)]["drwtNo" + str(drwt)])
+            refined[int(data[str(i)]["drwtNo" + str(drwt)])-1] = 1
+        refined[int(data[str(i)]["bnusNo"])-1] = 1
         y.append(refined)
 
     x = np.array(x)
     y = np.array(y)
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1)
 
-    model = models.Sequential()
-    model.add(layers.Dense(6, input_shape=(1,)))
-    model.compile(optimizer='SGD', loss='mse', metrics=['accuracy'])
-
-    history = model.fit(x_train, y_train, epochs=30, verbose=1, validation_split=0.1)
-    [loss, accuracy] = model.evaluate(x_test, y_test, verbose=1)
-    print("Accuracy: " + str(accuracy*100) + "%")
-    print("=== " + str(drwNo) + "st Numbers Prediction ===")
+    model = models.Sequential([
+        layers.Dense(45, input_shape=(1,), activation='softmax'),
+    ])
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=10000, verbose=1, validation_split=0.1)
     result = model.predict([drwNo])
     print(result)
+    result = result[0].tolist()
+
+    numbers = []
+    for n in range(1, 7):
+        idx = result.index(max(result))
+        result[idx] = -1
+        numbers.append(idx+1)
+
+    print("=== " + str(drwNo) + "st Numbers Prediction ===")
+    print(numbers)
