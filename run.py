a5cf292f9d51f6c09a3f5d86e6b837a085cae1dd
6londe <6londe@gmail.com>
2020-03-30 20:26:37 +0900

    Fix algorithm

--- run.py
+++ run.py
@@ -31,7 +31,7 @@ def get_data():
 
     return data, drwNo
 
-def get_numbers(drwNo):
+def get_numbers_from_drwtNo(drwNo):
 
     x = []
     y = []
@@ -51,8 +51,45 @@ def get_numbers(drwNo):
         layers.Dense(45, input_shape=(1,), activation='softmax'),
     ])
     model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-    model.fit(x_train, y_train, epochs=1000, verbose=1, validation_split=0.1)
+    model.fit(x_train, y_train, epochs=10, verbose=1, validation_split=0.1)
     result = model.predict([drwNo])
+    print(result)
+    result = result[0].tolist()
+    return result
+
+def get_numbers(drwNo):
+
+    x = []
+    y = []
+
+    refined = np.zeros(45)
+    for drwt in range(1, 7):
+        refined[int(data["1"]["drwtNo" + str(drwt)])-1] = 1
+        refined[int(data["1"]["bnusNo"])-1] = 1
+    x.append(refined)
+    
+    for i in range(1, drwNo):
+        refined = np.zeros(45)
+        for drwt in range(1, 7):
+            refined[int(data[str(i)]["drwtNo" + str(drwt)])-1] = 1
+        refined[int(data[str(i)]["bnusNo"])-1] = 1
+        y.append(refined)
+        if i == (drwNo-1):
+            infer_input = refined
+            break
+        x.append(refined)
+
+    x = np.array(x)
+    y = np.array(y)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1)
+
+    model = models.Sequential([
+        layers.Dense(45, input_shape=(45,), activation='softmax'),
+    ])
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=1000, verbose=1, validation_split=0.1)
+
+    result = model.predict(np.array([infer_input]))
     print(result)
     result = result[0].tolist()
     return result
